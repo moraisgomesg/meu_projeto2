0a480f10af6b01eccf95d775065ba18ef17e4b65
moraisgomesg <geylsonmg@icloud.com>
2024-10-17 19:47:08 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" 
+    content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da maldiçao</title>
+</head>
+<body>
+    <H1>
+       <?php 
+    echo "Ola mundo! \u{1F30E}";
+       
+       ?>
+
+    </H1>
+    <p>Vamos tentar nos livrar da maldiçao</p> 
+</body>
+</html>
